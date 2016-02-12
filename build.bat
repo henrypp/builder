45dab98dbca4245a909c0b210f1f2f5ed865a848
henrypp <sforce5@mail.ru>
2016-02-13 01:39:15 +0600

    fixes

--- build.bat
+++ build.bat
@@ -10,9 +10,11 @@ set "TMP_DIRECTORY=%~dp0tmp"
 set "NSIS_BIN="%~dp0\nsis\makensis.exe""
 
 rem Cleanup
+
 del /s /f /q "%TMP_DIRECTORY%\*"
 
 rem Create temporary folder with binaries and documentation...
+
 mkdir "%TMP_DIRECTORY%\32\i18n"
 mkdir "%TMP_DIRECTORY%\64\i18n"
 
@@ -30,6 +32,7 @@ copy /y "%BIN_DIRECTORY%\History.txt" "%TMP_DIRECTORY%\History.txt"
 copy /y "%BIN_DIRECTORY%\License.txt" "%TMP_DIRECTORY%\License.txt"
 
 rem Pack portable version
+
 7z.exe a -mx9 "%OUT_DIRECTORY%\%APP_NAME_SHORT%-%APP_VERSION%-bin.7z" "%TMP_DIRECTORY%\*"
 
 rem Pack installer
@@ -43,8 +46,11 @@ copy /y "%BIN_DIRECTORY%\i18n" "%TMP_DIRECTORY%\i18n"
 %NSIS_BIN% /DAPP_NAME="%APP_NAME%" /DAPP_FILES_DIR="%TMP_DIRECTORY%" /DAPP_NAME_SHORT="%APP_NAME_SHORT%" /DAPP_VERSION="%APP_VERSION%" /X"OutFile %OUT_DIRECTORY%\%APP_NAME_SHORT%-%APP_VERSION%-setup.exe" installer.nsi
 
 rem Cleanup
+
 rmdir /s /q "%TMP_DIRECTORY%\32"
 rmdir /s /q "%TMP_DIRECTORY%\64"
+rmdir /s /q "%TMP_DIRECTORY%\i18n"
+
 del /s /f /q "%TMP_DIRECTORY%\*"
 
 pause
