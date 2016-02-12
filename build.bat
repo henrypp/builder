0ebe3384f8a5ef39913b4648ba257f3b137348d0
henrypp <sforce5@mail.ru>
2016-02-13 02:02:25 +0600

    extended script

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ set "APP_VERSION=3.1.1452"
 set "BIN_DIRECTORY=%~dp0..\%APP_NAME_SHORT%\bin"
 set "OUT_DIRECTORY=%~dp0bin"
 set "TMP_DIRECTORY=%~dp0tmp"
-set "NSIS_BIN="%~dp0\nsis\makensis.exe""
+set "NSIS_BIN=%~dp0\nsis\makensis.exe"
 
 rem Cleanup
 
@@ -16,17 +16,33 @@ del /s /f /q "%TMP_DIRECTORY%\*"
 
 rem Create temporary folder with binaries and documentation...
 
-mkdir "%TMP_DIRECTORY%\32\i18n"
-mkdir "%TMP_DIRECTORY%\64\i18n"
+mkdir "%TMP_DIRECTORY%\32"
+mkdir "%TMP_DIRECTORY%\64"
 
-copy /y "%BIN_DIRECTORY%\i18n" "%TMP_DIRECTORY%\32\i18n"
-copy /y "%BIN_DIRECTORY%\i18n" "%TMP_DIRECTORY%\64\i18n"
+copy /y "%BIN_DIRECTORY%\*.bat" "%TMP_DIRECTORY%\32\*.bat"
+copy /y "%BIN_DIRECTORY%\*.reg" "%TMP_DIRECTORY%\32\*.reg"
+
+copy /y "%BIN_DIRECTORY%\*.bat" "%TMP_DIRECTORY%\64\*.bat"
+copy /y "%BIN_DIRECTORY%\*.reg" "%TMP_DIRECTORY%\64\*.reg"
+
+if exist "%BIN_DIRECTORY%\i18n" (
+	mkdir "%TMP_DIRECTORY%\32\i18n"
+	mkdir "%TMP_DIRECTORY%\64\i18n"
+	
+	copy /y "%BIN_DIRECTORY%\i18n" "%TMP_DIRECTORY%\32\i18n"
+	copy /y "%BIN_DIRECTORY%\i18n" "%TMP_DIRECTORY%\64\i18n"
+)
 
 copy /y "%BIN_DIRECTORY%\32\%APP_NAME_SHORT%.exe" "%TMP_DIRECTORY%\32\%APP_NAME_SHORT%.exe"
 copy /y "%BIN_DIRECTORY%\64\%APP_NAME_SHORT%.exe" "%TMP_DIRECTORY%\64\%APP_NAME_SHORT%.exe"
 
-echo. 2>"%TMP_DIRECTORY%\32\%APP_NAME_SHORT%.ini"
-echo. 2>"%TMP_DIRECTORY%\64\%APP_NAME_SHORT%.ini"
+if exist "%BIN_DIRECTORY%\%APP_NAME_SHORT%.ini" (
+	copy /y "%BIN_DIRECTORY%\%APP_NAME_SHORT%.ini" "%TMP_DIRECTORY%\32\%APP_NAME_SHORT%.ini"
+	copy /y "%BIN_DIRECTORY%\%APP_NAME_SHORT%.ini" "%TMP_DIRECTORY%\64\%APP_NAME_SHORT%.ini"
+) else (
+	echo. 2>"%TMP_DIRECTORY%\32\%APP_NAME_SHORT%.ini"
+	echo. 2>"%TMP_DIRECTORY%\64\%APP_NAME_SHORT%.ini"
+)
 
 copy /y "%BIN_DIRECTORY%\Readme.txt" "%TMP_DIRECTORY%\Readme.txt"
 copy /y "%BIN_DIRECTORY%\History.txt" "%TMP_DIRECTORY%\History.txt"
@@ -38,13 +54,17 @@ rem Pack portable version
 
 rem Pack installer
 
-rmdir /s /q "%TMP_DIRECTORY%\32\i18n"
-rmdir /s /q "%TMP_DIRECTORY%\64\i18n"
-
-mkdir "%TMP_DIRECTORY%\i18n"
-copy /y "%BIN_DIRECTORY%\i18n" "%TMP_DIRECTORY%\i18n"
-
-%NSIS_BIN% /DAPP_FILES_DIR="%TMP_DIRECTORY%" /DAPP_NAME="%APP_NAME%" /DAPP_NAME_SHORT="%APP_NAME_SHORT%" /DAPP_VERSION="%APP_VERSION%" /X"OutFile %OUT_DIRECTORY%\%APP_NAME_SHORT%-%APP_VERSION%-setup.exe" installer.nsi
+if exist "%NSIS_BIN%" (
+	rmdir /s /q "%TMP_DIRECTORY%\32\i18n"
+	rmdir /s /q "%TMP_DIRECTORY%\64\i18n"
+	
+	if exist "%BIN_DIRECTORY%\i18n" (
+		mkdir "%TMP_DIRECTORY%\i18n"
+		copy /y "%BIN_DIRECTORY%\i18n" "%TMP_DIRECTORY%\i18n"
+	)
+	
+	"%NSIS_BIN%" /DAPP_FILES_DIR="%TMP_DIRECTORY%" /DAPP_NAME="%APP_NAME%" /DAPP_NAME_SHORT="%APP_NAME_SHORT%" /DAPP_VERSION="%APP_VERSION%" /X"OutFile %OUT_DIRECTORY%\%APP_NAME_SHORT%-%APP_VERSION%-setup.exe" installer.nsi
+)
 
 rem Cleanup
 
