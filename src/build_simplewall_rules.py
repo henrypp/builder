c78666c320f480963f0e8a91a17cc3258fcbc823
henrypp <support@henrypp.org>
2022-05-17 16:24:24 +0600

    upd rules builder

--- src/build_simplewall_rules.py
+++ src/build_simplewall_rules.py
@@ -1,40 +1,131 @@
 import xml.dom.minidom
+import hashlib
 
 import argparse
 from helper import *
 
 parser = argparse.ArgumentParser (add_help=False, description='Update internal rules.')
-parser.add_argument ('--name-short', help='project short name', required=True)
+parser.add_argument ('--mode', help='working mode (update - update wsb rules/unpack/pack)', required=True)
+parser.add_argument ('--i', help='in file', required=True)
+parser.add_argument ('--o', help='out file', required=True)
 
 args = parser.parse_args ()
 
-APP_NAME_SHORT=args.name_short
+ARG_MODE=args.mode
+ARG_FILE_IN=args.i
+ARG_FILE_OUT=args.o
 
 CURRENT_DIRECTORY = os.path.dirname (os.path.abspath (__file__))
-RULES_DIR = os.path.join (CURRENT_DIRECTORY, '..', '!repos', 'WindowsSpyBlocker', 'data', 'firewall')
-PROJECT_DIRECTORY = os.path.join (CURRENT_DIRECTORY, '..', APP_NAME_SHORT)
-RULES_FILE = os.path.join (PROJECT_DIRECTORY, 'bin', 'profile_internal.xml')
-RULES_FILE_PACKED = os.path.join (PROJECT_DIRECTORY, 'bin', 'profile_internal_packed.bin')
+PROJECT_DIRECTORY = os.path.join (CURRENT_DIRECTORY, '..', '..', ARG_FILE_IN)
+WSB_DIR = os.path.join (CURRENT_DIRECTORY, '..', '..', '!repos', 'WindowsSpyBlocker', 'data', 'firewall')
 
-check_path_with_status ('Profile directory', PROJECT_DIRECTORY)
-check_path_with_status ('WSB directory', RULES_DIR)
-check_path_with_status ('Profile file', RULES_FILE, True)
+# File information
+PROFILE2_FOURCC = b'\x73\x77\x63\x31'
+PROFILE2_FOURCC_LENGTH = len (PROFILE2_FOURCC)
+PROFILE2_HASH_LENGTH = 32
 
-# Open profile xml
-with open (RULES_FILE, 'r', newline='') as f:
-	data = f.read ()
+check_path_with_status ('Profile file', ARG_FILE_IN, True)
+check_path_with_status ('WSB directory', WSB_DIR)
 
-	if not data:
-		raise Exception ('File reading failure: ' + RULES_FILE)
+def get_hash (buffer):
+	return hashlib.sha256 (buffer).digest ()
 
+def is_valid_hash (buffer, compare_hash):
+	current_hash = get_hash (buffer)
+
+	return (current_hash == compare_hash)
+
+def is_valid_signature (signature):
+	return (signature == PROFILE2_FOURCC)
+
+def parse_xml (data):
 	# toprettyxml() hack
 	data = data.replace ('\n', '')
 	data = data.replace ('\t', '')
 
 	xml_doc = xml.dom.minidom.parseString (data)
-	xml_root = xml_doc.getElementsByTagName ("root")
+	xml_root = xml_doc.getElementsByTagName ('root')
 
-	f.close ()
+	return xml_doc, xml_root
+
+def save_file (file_path, buffer):
+	with open (file_path, 'wb') as fn:
+		fn.write (buffer)
+		fn.close ()
+
+def decode_profile (file_path):
+	with open (file_path, 'rb') as fn:
+
+		# read file size
+		fn.seek (0, os.SEEK_END)
+
+		if fn.tell () <= (PROFILE2_FOURCC_LENGTH + PROFILE2_HASH_LENGTH):
+			log_status (status.FAILED, 'Length failure: "' + get_file_name (file_path) + '"')
+			fn.close ()
+
+			return None
+
+		fn.seek (0, os.SEEK_SET) # restore position
+
+		# read signature value
+		signature_value = fn.read (PROFILE2_FOURCC_LENGTH)
+
+		if not is_valid_signature (signature_value):
+			log_status (status.FAILED, 'Signature failure: "' + get_file_name (file_path) + '"')
+			fn.close ()
+
+			return None
+
+		# read sha256 value
+		hash_value = fn.read (PROFILE2_HASH_LENGTH)
+
+		lznt_buffer = fn.read ()
+		fn.close ()
+
+		buffer_data = (len (lznt_buffer) * ctypes.c_ubyte).from_buffer_copy (lznt_buffer)
+		buffer_data = unpack_buffer_lznt (buffer_data)
+
+		if not buffer_data:
+			log_status (status.FAILED, 'Decompression failure: "' + get_file_name (file_path) + '"')
+			return None
+
+		if not is_valid_hash (buffer_data, hash_value):
+			log_status (status.FAILED, 'Hash failure: "' + get_file_name (file_path) + '"')
+			return None
+
+		return buffer_data
+
+def encode_profile (file_path, hash_value, xml_buffer):
+	buffer_length = len (xml_buffer)
+	buffer_data = (buffer_length * ctypes.c_ubyte).from_buffer_copy (xml_buffer)
+
+	buffer_data = pack_buffer_lznt (buffer_data)
+
+	if not buffer_data:
+		log_status (status.FAILED, 'Compression failed...')
+		return
+
+	log_status (status.SUCCESS, 'Compress buffer with %s size to %s (%s saved)...' % (format_size (buffer_length), format_size (len (buffer_data)), format_size (buffer_length - len (buffer_data))))
+
+	with open (file_path, 'wb') as fn:
+		fn.write (PROFILE2_FOURCC) # compressed profile signature
+		fn.write (hash_value) # sha256 hash of uncompressed data
+
+		fn.write (buffer_data)
+
+		fn.close ()
+
+# Start parse
+buffer = decode_profile (ARG_FILE_IN)
+
+if not buffer:
+	raise Exception ('Unpacking failure.')
+
+buffer_hash = get_hash (buffer)
+
+buffer = bytes (buffer).decode ('utf-8')
+
+xml_doc, xml_root = parse_xml (buffer)
 
 # Store timestamp
 timestamp = int (xml_root[0].getAttribute ('timestamp'))
@@ -53,26 +144,26 @@ if not xml_section:
 total_rules_count = 0
 
 # Enumerate Windows Spy Blocker spy/extra and update rules
-for file_name in os.listdir (RULES_DIR):
+for file_name in os.listdir (WSB_DIR):
 	module_name = os.path.splitext (file_name)[0]
-	module_path = os.path.join (RULES_DIR, file_name)
+	module_path = os.path.join (WSB_DIR, file_name)
 
 	log_status (status.TITLE, 'Parsing "' + module_name + '" rules')
 
-	lastmod = int (os.path.getmtime (module_path));
+	lastmod = int (os.path.getmtime (module_path))
 
 	if lastmod > timestamp:
-		timestamp = lastmod;
+		timestamp = lastmod
 
-	with open (module_path, 'r') as f:
-		rows = f.readlines ()
+	with open (module_path, 'r') as fn:
+		rows = fn.readlines ()
 		natural_sort (rows)
-		f.close ()
+		fn.close ()
 
 		count = 0
 
 		for string in rows:
-			line = string.strip ("\n\r\t ")
+			line = string.strip ('\n\r\t ')
 
 			if not line or line.startswith ('#') or line.startswith ('<') or line.startswith ('>') or line.startswith ('='):
 				continue
@@ -86,26 +177,28 @@ for file_name in os.listdir (RULES_DIR):
 
 			xml_section[0].appendChild (new_item)
 
-		log_status (status.SUCCESS, 'Found %i %s rules...' % (count, module_name))
+		if count != 0:
+			log_status (status.SUCCESS, 'Found %i %s rules...' % (count, module_name))
+		else:
+			log_status (status.WARNING, 'Not found %s rules...' % (module_name))
 
 		total_rules_count += count
 
 # Set new rule timestamp
 xml_root[0].setAttribute ('timestamp', str (timestamp))
 
-# Save updated profile xml
-data = xml_doc.toprettyxml (indent="\t", newl="\n")
+# Generate xml string
+data = xml_doc.toprettyxml (indent='\t', newl='\n')
 
-if data:
-	data = data.replace ('/>', ' />')
+if not data:
+	raise Exception ('Write xml failure.')
 
-	with open (RULES_FILE, 'w', newline='') as fn:
-		fn.write (data)
-		fn.close ()
+data = data.replace ('/>', ' />')
+data = bytes (data, 'utf-8')
 
-# Compress file
+# Save profile file
 log_status (status.TITLE, 'Compress blocklist with LZNT1')
 
-compress_file_lznt (RULES_FILE, RULES_FILE_PACKED)
+encode_profile (ARG_FILE_OUT, buffer_hash, data)
 
-print ('\nBlocklist rules count: %s\nBlocklist timetamp: %s\n' % (str (total_rules_count), str (timestamp)))
+print ('\nBlocklist rules count: %s\nBlocklist timestamp: %s\n' % (str (total_rules_count), str (timestamp)))
